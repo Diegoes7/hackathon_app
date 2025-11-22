f0ed6c315d992f166b354f6a09477a8f77a299f6
DiegoElRey <d.dimitrov@bpmhub.com>
2025-11-22 22:13:22 +0200

    fix a error

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
 import Dashboard from './components/dashboard/Dashboard';
-import UserInfo from './components/userInfo/UserInfo';
 
 function App() {
 	return (
 		<div className='App'>
 			<h1>Добре дошли в Пазител</h1>
-			{/* <UserInfo /> */}
 			<Dashboard />
-			{/* <MentalHealthReport /> */}
 		</div>
 	);
 }
